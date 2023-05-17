50c102c0a5ba81a561aa73ae3f64cd1d497352fa
donghyun <kdhdh001@naver.com>
2023-05-18 00:35:54 +0900

    add posterview

--- final-pjt-front/src/components/MovieListItem.vue
+++ final-pjt-front/src/components/MovieListItem.vue
@@ -1,10 +1,15 @@
 <template>
   <div>
-    <h5>영화제목 {{movie.title}}</h5>
+    <div>
+      <img :src="`https://image.tmdb.org/t/p/original/${movie.poster_path}`" alt="" style="width:300px; height:350px">
+      <h5>영화제목 {{movie.title}}</h5>
+    </div>
   </div>
 </template>
 
 <script>
+
+
 export default {
   name: 'MovieListItem',
   props: {
