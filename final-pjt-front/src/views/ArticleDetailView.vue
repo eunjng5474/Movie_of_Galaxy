a60b944fcdb5770b56022c887a47bcaff614031b
eunjng5474 <eunjng5474@gmail.com>
2023-05-18 21:41:41 +0900

    add article delete and change user model

--- final-pjt-front/src/views/ArticleDetailView.vue
+++ final-pjt-front/src/views/ArticleDetailView.vue
@@ -1,17 +1,18 @@
 <template>
   <div>
     <h1>Article Detail</h1>
-    <h3>{{detailOneArticle.title}}</h3>
-    <p>{{detailOneArticle.content}}</p>
+    <h3>{{detailOneArticle?.title}}</h3>
+    <p>{{detailOneArticle?.content}}</p>
     <!-- {{detailOneArticle.id}} -->
     <router-link :to="{ name: 'ArticleUpdateView', 
     params: {id: detailOneArticle.id}}">[UPDATE]</router-link>
+    <button @click="deleteArticle">DELETE</button>
   </div>
 </template>
 
 <script>
-// import axios from 'axios'
-// const API_URL = 'http://127.0.0.1:8000'
+import axios from 'axios'
+const API_URL = 'http://127.0.0.1:8000'
 
 
 export default {
@@ -31,6 +32,18 @@ export default {
     getArticleDetail() {
       const articleId = this.$route.params.id
       this.$store.dispatch('getArticleDetail', articleId)
+    },
+    deleteArticle() {
+      axios({
+        method: 'delete',
+        url: `${API_URL}/api/v2/articles/${this.$route.params.id}/`,
+      })
+      .then(() => {
+        this.$router.push({ name: 'CommunityView' })
+      })
+      .catch((err) => {
+        console.log(err)
+      })
     }
   }
 }
