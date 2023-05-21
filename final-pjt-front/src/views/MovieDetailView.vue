6d054daaf96bc283f3b30e1b935fd384938851fe
eunjng5474 <eunjng5474@gmail.com>
2023-05-21 11:07:06 +0900

    movie detail css -ing

--- final-pjt-front/src/views/MovieDetailView.vue
+++ final-pjt-front/src/views/MovieDetailView.vue
@@ -3,17 +3,19 @@
     <NavBar/>
     <h1>무비상세페이지</h1>
     <div class="container">
+      <div class="justify-content-center" style="width: 500px">
       <img :src="`https://image.tmdb.org/t/p/original/${detailOneMovie?.poster_path}`" alt="" style="width:300px; height:350px">
       <h3>{{detailOneMovie?.title}}</h3>
-      <p>{{detailOneMovie?.overview}}</p>
-      <button class="btn btn-outline-primary" @click="likeMovie">좋아요</button>
-      <br>
-      <!-- 임시 - 좋아요 한 유저 -->
-      {{ detailOneMovie?.like_users}}
-      {{ detailOneMovie?.vote_average }}
+        <p>{{detailOneMovie?.overview}}</p>
+        <button :class="like_btn" @click="likeMovie">좋아요</button>
+        <br>
+        <!-- 임시 - 좋아요 한 유저 -->
+        {{ detailOneMovie?.like_users}}
+        {{ detailOneMovie?.vote_average }}
       <br>
       <!-- 유튜브 예고편 보여줄 곳 -->
       <iframe :src="`https://www.youtube.com/embed/${detailOneMovie?.key}?autoplay=1&mute=1`" frameborder="0" style="width: 500px; height: 300px"></iframe>
+      </div>
     </div>
 
   </div>
@@ -31,7 +33,13 @@ export default {
   components: {
     NavBar
   },
-
+  data() {
+    return {
+      // click_btn: 'btn btn-primary',
+      like_btn: 'btn btn-outline-primary',
+      like_toggle: false
+    }
+  },
   created() {
     this.getMovieDetail()
   },
@@ -40,6 +48,9 @@ export default {
     detailOneMovie() {
       // console.log(this.$route.params)
       return this.$store.getters.detailmovie
+    },
+    getCurrentUser() {
+      return this.$store.getters.currentUser
     }
   },
 
@@ -51,8 +62,17 @@ export default {
 
     },
     likeMovie() {
+      // 좋아요 누르면 새로고침 해야 유저 아이디 뜸
+      // 지금은 필요없지만 추후 getmoviedetail 해야할지도
+      // 좋아요 누르고 새로고침하면 버튼 색깔 다시 기본으로 돌아감 - this.like_toggle로 판단해서인 듯
       const movieId = this.$route.params.id
       this.$store.dispatch('likeMovie', movieId)
+      this.like_toggle = !this.like_toggle
+      if (this.like_toggle === true) {
+        this.like_btn = 'btn btn-primary'
+      } else {
+        this.like_btn = 'btn btn-outline-primary'
+      }
     },
     // getYoutube() {
     //   axios({
